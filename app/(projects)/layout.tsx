87e1d111c7b1f60f17eff93cc436e56c4abc885f
Tom Hackshaw <gh@tomhackshaw.com>
2024-05-31 13:15:44 +1200

    OG fix?

--- app/(projects)/layout.tsx
+++ app/(projects)/layout.tsx
@@ -1,11 +1,36 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { og } from '@/config/site'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
 import type { Metadata } from 'next'
 import { ViewTransitions } from 'next-view-transitions'
 
 import '@/styles/globals.css'
+
+export const metadata: Metadata = {
+	title: og.title,
+	description: og.description,
+	keywords: og.keywords,
+	authors: og.authors,
+	creator: og.creator,
+	openGraph: og.openGraph,
+	twitter: og.twitter,
+	icons: og.icons,
+	metadataBase: new URL(og.url),
+	manifest: `${og.url}/site.webmanifest`,
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+}
 
 export default function RootLayout({
 	children,
